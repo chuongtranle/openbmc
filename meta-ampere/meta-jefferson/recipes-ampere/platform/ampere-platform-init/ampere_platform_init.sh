488f999057f383e7229e95a82636d07054b7a7d3
Hieu Huynh <hieuh@os.amperecomputing.com>
2024-12-03 02:44:48 +0000

    meta-ampere: mtjefferson: set Mpro0 console as default
    As hardware design, the BMC_GPIO18B7_S01_UART1_SEL GPIO should be set to
    1 to select the Mpro0 console.
    
    Tested:
    1. Switch UART mux to BMC
       $ ampere_uartmux_ctrl.sh 2 2
    2. Open SOLSSH console for Mpro0
       $ ssh -p 2201 root@<BMC_IP>
    3. Verify that Mpro0 console can show logs when Host power on
    
    Signed-off-by: Hieu Huynh <hieuh@os.amperecomputing.com>

--- meta-ampere/meta-jefferson/recipes-ampere/platform/ampere-platform-init/ampere_platform_init.sh
+++ meta-ampere/meta-jefferson/recipes-ampere/platform/ampere-platform-init/ampere_platform_init.sh
@@ -72,6 +72,7 @@ gpioset $(gpiofind hpm-fw-recovery)=0      # Force HPM FPGA recovery
 gpioset $(gpiofind spi-nor-access)=0       # Deassert BMC access SPI-NOR pin
 gpioset $(gpiofind host0-special-boot)=0   # Deassert SPECIAL_BOOT GPIO pin
 gpioset $(gpiofind cpu-bios-recover)=0     # BIOS recovery enable from BMC
+gpioset $(gpiofind s01-uart1-sel)=1        # Select Mpro0 as defaut
 
 # When BMC is rebooted, because PSON_L has pull up to P3V3_STB, it changes its
 # value to HIGH. Add code to check P3V3_STB and recover PSON_L to correct state
